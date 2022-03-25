b4c0910898d2559ed05c0678e6c151bdf7890b5a
Arun Kumar <arun.kumar@dematic.com>
2022-03-25 11:18:36 -0400

    WIP - Init landmarks

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,16 @@
 #include "sciplot/sciplot.hpp"
 #include "NumCpp.hpp"
+#include "fmt/color.h"
 #include <string>
 #include <vector>
 #include <iterator>
 #include <iostream>
 #include <memory>
+#include "tuple"
 
 using namespace sciplot;
+
+typedef nc::NdArray<double> ncD;
 
 struct slam_parameters {
     double sig_x = 0.25;
@@ -18,74 +22,88 @@ struct slam_parameters {
     const double sig_y2 = std::pow(sig_y, 2);
     const double sig_alpha2 = std::pow(sig_alpha, 2);
     const double sig_beta2 = std::pow(sig_beta, 2);
-    const double sig_r2 = std::pow(sig_r,2);
+    const double sig_r2 = std::pow(sig_r, 2);
 };
-
-//struct slam_data {
-//    std::vector<float> measurement;
-//    std::vector<float> control;
-//};
 
 struct slam_data_np {
-    nc::NdArray<double> measurement;
-    nc::NdArray<double> control;
+    ncD measurement;
+    ncD control;
 };
 
-int main(int argc, char** argv)
-{
+void process_input_data(std::vector<std::unique_ptr<slam_data_np>> &slam_data_vec_np) {
     std::string FILENAME = "data.txt";
     std::vector<double> vec_buffer;
-//    std::unique_ptr<slam_data> slamDataPtr = nullptr;
     std::unique_ptr<slam_data_np> slamDataNpPtr = nullptr;
-//    std::vector<std::unique_ptr<slam_data>> slam_data_vec;
-    std::vector<std::unique_ptr<slam_data_np>> slam_data_vec_np;
     std::ifstream file(FILENAME);
     if (file.is_open()) {
         std::string line;
         while (std::getline(file, line)) {
             std::istringstream iss(line);
             vec_buffer.clear();
-            std::copy(std::istream_iterator<float>(iss),
-                      std::istream_iterator<float>(),
+            std::copy(std::istream_iterator<double>(iss),
+                      std::istream_iterator<double>(),
                       std::back_inserter(vec_buffer));
             if (vec_buffer.size() > 2) {
-//                slamDataPtr = std::make_unique<slam_data>();
-//                slamDataPtr->measurement.assign(vec_buffer.begin(), vec_buffer.end());
                 slamDataNpPtr = std::make_unique<slam_data_np>();
-                slamDataNpPtr->measurement = nc::NdArray<double>(1, vec_buffer.size());
+                slamDataNpPtr->measurement = ncD(1, vec_buffer.size());
                 for (nc::int32 col = 0; col < vec_buffer.size(); ++col) {
                     slamDataNpPtr->measurement(0, col) = vec_buffer[col];
                 }
             } else {
-//                slamDataPtr->control.assign(vec_buffer.begin(), vec_buffer.end());
-                slamDataNpPtr->control = nc::NdArray<double>(1, vec_buffer.size());
+                slamDataNpPtr->control = ncD(1, vec_buffer.size());
                 for (nc::int32 col = 0; col < vec_buffer.size(); ++col) {
                     slamDataNpPtr->control(0, col) = vec_buffer[col];
                 }
-//                slam_data_vec.push_back(std::move(slamDataPtr));
                 slam_data_vec_np.push_back(std::move(slamDataNpPtr));
-//                slamDataPtr.reset();
                 slamDataNpPtr.reset();
             }
         }
         file.close();
     }
-//    std::cout << "Size of slam data is " << slam_data_vec.size() << "\n";
-//    for (auto& item: slam_data_vec) {
-//        std::cout<< "Measurement data\n";
-//        for (auto& element : item->measurement) {
-//            std::cout << element << "\t" << std::endl;
-//        }
-//        std::cout << "Control data\n";
-//        for (auto& element : item->control) {
-//            std::cout << element << "\t" << std::endl;
-//        }
-//    }
+}
 
-    for (auto &item: slam_data_vec_np) {
-        std::cout << "NumCpp measurement data :\n";
-        item->measurement.print();
-        std::cout << "NumCpp control data :\n";
-        item->control.print();
-    }
+std::tuple<int, ncD, ncD> init_landmarks(ncD &measurement, ncD &measurement_cov, ncD &pose, ncD &pose_cov) {
+    int k = std::floor(measurement.shape().rows/2);
+    double x, y, theta;
+    ncD Z;
+    x = pose(0,0);
+    y = pose(1,0);
+    theta = pose(2,0);
+    Z = measurement.reshape(6,2);
+    Z.print();
+    return std::make_tuple(1, nc::zeros<double>(1,1),  nc::zeros<double>(1,1));
+}
+
+int main(int argc, char **argv) {
+    std::vector<std::unique_ptr<slam_data_np>> slam_data_vec_np;
+    process_input_data(slam_data_vec_np);
+//    for (auto &item: slam_data_vec_np) {
+//        fmt::print(fg(fmt::color::crimson) | fmt::emphasis::bold,
+//                   "NumCpp measurement data :\n");
+//        item->measurement.print();
+//        std::cout << "NumCpp control data :\n";
+//        item->control.print();
+//    }
+    auto measurement = slam_data_vec_np[0]->measurement;
+    measurement.reshape(12, 1);
+    std::cout << measurement.shape();
+    int time_step = 1;
+    slam_parameters param;
+    auto control_cov = ncD(3, 3);
+    control_cov = nc::eye<double>(3);
+    control_cov(0,0) = param.sig_x2;
+    control_cov(1,1) = param.sig_y2;
+    control_cov(2,2) = param.sig_alpha2;
+    auto measurement_cov = ncD(2, 2);
+    measurement_cov = nc::eye<double>(2);
+    measurement_cov(0,0) = param.sig_beta2;
+    measurement_cov(1,1) = param.sig_r2;
+    auto pose = nc::zeros<double>(3,1);
+    auto pose_cov = ncD(3,3);
+    pose_cov = nc::eye<double>(3);
+    pose_cov(0,0) = std::pow(0.02, 2);
+    pose_cov(1,1) = std::pow(0.02, 2);
+    pose_cov(2,2) = std::pow(0.1, 2);
+    init_landmarks(measurement, measurement_cov, pose, pose_cov);
+    return 0;
 }
